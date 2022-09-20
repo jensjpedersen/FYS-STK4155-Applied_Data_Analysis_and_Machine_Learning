449e11189bc2bf6f109b14dffa443585c2ca97c3
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-09-20 11:25:49 +0200

    Plot beta

--- Project1/plot.py
+++ Project1/plot.py
@@ -26,6 +26,8 @@ class Plot:
         assert(np.size(self.y_train) > np.size(self.y_test))
         self.poly_deg = np.array([ int(i) for i in self.poly_score.keys() ])
 
+
+
     def plot_mse(self): 
         try:
             keys = self.poly_score['1']['mse'].keys()
@@ -71,9 +73,43 @@ class Plot:
                   'Update dict and re-run constructor.')
             return
 
+        n_features = np.shape(self.X_test)[1]
+
+        f = plt.figure()
+
+        style = ['bo', 'ro', 'ko']
+        for i, label in enumerate(keys): 
+            for p in self.poly_deg:
+                betas = self.poly_score[str(p)]['beta'][label]
+                x_rep = np.ones(len(betas)) * p
+                if p == 1: 
+                    # Add label
+                    plt.plot(x_rep, betas, style[i], label = label)
+                else:
+                    plt.plot(x_rep, betas, style[i])
+
+        plt.legend()
+        plt.show()
+
+                
+
+        # XXX: plot horizontal lines
         # # beta = [ self.poly_score[str(j)]['beta'][j] ]
-        # for i in self.poly_deg:
-        #     e = self.poly_score[str(i)]['beta']
+        # for label in keys:
+        #     for j in range(n_features): 
+        #         betas = []
+        #         for i in self.poly_deg:
+        #             try: 
+        #                 beta_n = self.poly_score[str(i)]['beta'][label][j]
+        #             except IndexError: 
+        #                 break
+
+        #         betas.append(beta_n)
+        #         plt.plot(range(1,len(betas)+1), betas)
+        #         plt.show()
+
+
+
         #     print(e)
         #     breakpoint() 
 
@@ -95,16 +131,19 @@ if __name__ == '__main__':
     test_size = 0.2
 
 
-    f = franke_data.FrankeData(max_poly_deg, n_data, data_dim = 1, add_noise = 0.5)
+    f = franke_data.FrankeData(max_poly_deg, n_data, data_dim = 1, add_noise = 0)
     X_train, X_test, y_train, y_test = f.get_train_test_data(test_size = test_size)
 
     score_list = ['mse', 'r2', 'beta']
-    method_list = ['ols_own', 'ols_own']
-    data_list = ['train', 'test']
+    method_list = ['ols_own', 'ols_own', 'ols_skl']
+    method_list = ['ols_skl']
+    method_list = ['ols_own']
+    data_list = ['train', 'test', 'test']
+    data_list = ['test']
 
     a = analysis.Analysis(X_train, X_test, y_train, y_test)
     score = a.calculate_loop(max_poly_deg, score_list, method_list, data_list)
 
     p = Plot(X_train, X_test, y_train, y_test, score)
-    p.plot_beta()
+    p.plot_mse()
 
