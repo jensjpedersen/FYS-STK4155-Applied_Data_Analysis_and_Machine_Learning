7cb05c50683286b03045ca4c6466f0ac2cd036f9
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-09-21 16:52:51 +0200

    Moved plotting methods to plot_model.py

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -25,7 +25,7 @@ def relative_errror(y_data, y_model):
     return abs((y_data-y_model)/y_data)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Analysis: 
     X_train: np.ndarray
     X_test: np.ndarray
@@ -68,7 +68,7 @@ class Analysis:
             data_list = []
 
         # Slice desing matrix dependent og polynomal degree. 
-        l = round(((deg+1)*(deg+2)/2))		# Number of elements in beta
+        l = int(((deg+1)*(deg+2)/2))		# Number of elements in beta
 
         if l > np.shape(self.X_test)[1]: 
             raise ValueError("""Polynomail degree = {deg} requires {l} features in desing matrix, 
@@ -173,121 +173,28 @@ class Analysis:
         return beta_ols
 
 
-    def __franke_funciton(self, x: np.ndarray, y:np.ndarray):
-        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-        return term1 + term2 + term3 + term4
-
-    def plot_model(self, method: str, data: str, data_dim: int): 
-
-        x_train = self.X_train[:, 1] 
-        y_train = self.X_train[:, 2]
-        
-        o = ols.OLS(self.X_train, self.y_train)
-        if method == 'ols_own': 
-            o.ols()
-        elif method == 'ols_skl': 
-            o.skl_ols()
-        else:
-            raise ValueError('Valid methods are: "ols_own", "ols_skl"')
-
-
-        if data == 'train':
-            y_tilde = o.predict(self.X_train)
-            x_predict = self.X_train[:, 1]
-            y_predict = self.X_train[:, 2]
-            z_predict = self.y_train
-        elif data == 'test': 
-            y_tilde = o.predict(self.X_test)
-            x_predict = self.X_test[:, 1]
-            y_predict = self.X_test[:, 2]
-            z_predict = self.y_test
-
-        # if data_dim == 2:
-        #     N = np.sqrt(len(x))
-        #     x = x.reshape(N, N)
-        #     y = y.reshape(N, N)
-        #     z = z.reshape(N, N)
-        #     z_train_data = self.y_train
-        #     z_model = self.predict(X)
-
-
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        if data_dim == 1: 
-
-            # Plot datapoint used for model prediction 
-            x_sort_index = np.argsort(x_predict)
-            y_sort_index = np.argsort(y_predict)
-            assert(np.equal(x_sort_index, y_sort_index).all())
-            x = x_predict[x_sort_index]
-            y = y_predict[y_sort_index]
-            assert(np.equal(x_sort_index, y_sort_index).all())
-            z = z_predict[x_sort_index]
-            z_model = y_tilde[x_sort_index]
-
-            # ax = fig.add_subplot(111, projection='3d')
-            ax.plot(x, y, z, linewidth = 2, label = 'Data points')
-            # Plot model 
-            ax.plot(x, y, z_model, label = 'Model')
-
-            # Plot Franke function from trianing data, without noise
-            x_sort_index = np.argsort(x_train)
-            y_sort_index = np.argsort(y_train)
-            assert(np.equal(x_sort_index, y_sort_index).all())
-
-            x = x_train[x_sort_index]
-            y = y_train[y_sort_index]
-            z = self.__franke_funciton(x, y)
-
-            ax.plot(x, y, z, linewidth = 2, label = 'Franke function train data')
-
-            plt.xlabel('x')
-            plt.ylabel('y')
-
-            plt.legend()
-            plt.show()
-            
-            sys.exit()
-
-        else:
-            raise NotImplementedError
-
-
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        # if data_dim == 2:
-        #     ax.plot_surface(x, y, z) 
-        # elif data_dim == 1:
-        #     ax.plot(x, y, z)
-        # plt.show()
-
-
-
-
-
 if __name__ == '__main__': 
 
 
 
-    max_poly_deg = 1
-    n_data = 200000
-    n_data = 20000
+    max_poly_deg = 3
+    n_data = 1000
+    # n_data = 2000000
     test_size = 0.2
+    noise = 0
 
-    f = franke_data.FrankeData(max_poly_deg, n_data, data_dim = 1, add_noise = 0)
+    # XXX:  fix noise plot 
+    f = franke_data.FrankeData(max_poly_deg, n_data, data_dim = 1, add_noise = noise)
     X_train, X_test, y_train, y_test = f.get_train_test_data(test_size = test_size) # XXX pass to function call
     
     # f.print_design_matrix()
 
+    # Do regression 
+    # get_pred
+    # pass pred and test to analysis 
 
-    a = Analysis(X_train, X_test, y_train, y_test)
-    a.plot_model(method = 'ols_own', data = 'train', data_dim = 1)
-
+    a = Analysis(X_train, X_test, y_train, y_test) 
+    # a.plot_model(method = 'ols_own', data = 'train', data_dim = 1)
 
     method = ['ols_own', 'ols_own', 'ols_skl']
     data = ['train', 'test', 'test']
---
+++ Project1/plot_model.py
@@ -0,0 +1,174 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass, field
+import seaborn as sns
+import importlib
+import franke_data
+import time
+import sys
+import ipdb
+import ols 
+import bdb
+importlib.reload(franke_data)
+importlib.reload(ols)
+plt.style.use('fivethirtyeight')
+
+
+@dataclass(frozen=True)
+class PlotModel: 
+    X_train: np.ndarray
+    X_test: np.ndarray
+    y_train: np.ndarray
+    y_test: np.ndarray
+
+    def __franke_funciton(self, x: np.ndarray, y:np.ndarray):
+        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+        return term1 + term2 + term3 + term4
+
+
+    def plot(self, method: str, data: str, data_dim: int): 
+
+        # if data_dim == 2:
+        #     N = np.sqrt(len(x))
+        #     x = x.reshape(N, N)
+        #     y = y.reshape(N, N)
+        #     z = z.reshape(N, N)
+        #     z_train_data = self.y_train
+        #     z_model = self.predict(X)
+
+
+        if data_dim == 1: 
+            # NOTE: Change behvaour with plot call args. 
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            self.__1d_plot_data_points('train', fig, ax)
+            self.__1d_plot_model(data, method, ax)
+            self.__1d_plot_franke_funciton(ax)
+
+        # elif data_dim == 2:
+
+        #     x = self.x 
+        #     y = self.y
+        #     z = self.z
+
+        #     if self.data_dim > 1:
+        #         z = z.reshape(self.N, self.N)
+
+        #     fig = plt.figure()
+        #     ax = fig.add_subplot(111, projection='3d')
+        #     if self.data_dim == 2:
+        #         ax.plot_surface(x, y, z) 
+        #     elif self.data_dim == 1:
+        #         ax.plot(x, y, z)
+        #     plt.show()
+
+        else:
+            raise NotImplementedError
+
+
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        plt.show()
+
+
+    def __1d_plot_data_points(self, dataset: str, fig, ax):
+        if dataset == 'train':
+            x_data = self.X_train[:, 1] 
+            y_data = self.X_train[:, 2]
+            z_data = self.y_train
+        elif dataset == 'test': 
+            x_data = self.X_test[:, 1]
+            y_data = self.X_test[:, 2]
+            z_data = self.y_test
+        else:
+            raise ValueError
+
+        # Plot datapoints used for model prediction 
+        sort_index = np.argsort(x_data)
+        x = x_data[sort_index]
+        y = y_data[sort_index]
+        z = z_data[sort_index]
+
+        # Plot z data
+        ax.plot(x, y, z, '.', markersize = 5, label = f'{dataset} data')
+
+
+    def __1d_plot_model(self, dataset: str, model: str, ax): 
+
+        if model == 'ols_own': 
+            o = ols.OLS(self.X_train, self.y_train)
+            o.ols()
+
+        elif model == 'ols_skl':
+            o = ols.OLS(self.X_train, self.y_train)
+            o.skl_ols()
+
+        else:
+            raise ValueError
+
+        if dataset == 'train':
+            z_model = o.predict(self.X_train)
+            x_data = self.X_train[:, 1] 
+            y_data = self.X_train[:, 2]
+        elif dataset == 'test': 
+            z_model = o.predict(self.X_test)
+            x_data = self.X_test[:, 1]
+            y_data = self.X_test[:, 2]
+        else:
+            raise ValueError
+
+        sort_index = np.argsort(x_data)
+        x = x_data[sort_index]
+        y = y_data[sort_index]
+        z = z_model[sort_index]
+
+        ax.plot(x, y, z, linewidth=2, label = f'{dataset} data prediction')
+
+    def __1d_plot_franke_funciton(self, ax): 
+        x_data = self.X_train[:, 1] 
+        y_data = self.X_train[:, 2]
+        sort_index = np.argsort(x_data)
+        x = x_data[sort_index]
+        y = y_data[sort_index]
+        z = self.__franke_funciton(x, y) 
+
+        ax.plot(x, y, z, linewidth=1, label = 'Franke fuction')
+
+
+    # def __2d_plot_model(): 
+    #     print('2d')
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # if data_dim == 2:
+    #     ax.plot_surface(x, y, z) 
+    # elif data_dim == 1:
+    #     ax.plot(x, y, z)
+    # plt.show()
+
+
+
+
+
+if __name__ == '__main__': 
+
+
+
+    max_poly_deg = 1
+    n_data = 1000
+    # n_data = 2000000
+    test_size = 0.2
+    noise = 0.01
+
+    # XXX:  fix noise plot 
+    f = franke_data.FrankeData(max_poly_deg, n_data, data_dim = 1, add_noise = noise)
+    X_train, X_test, y_train, y_test = f.get_train_test_data(test_size = test_size) # XXX pass to function call
+
+    a = PlotModel(X_train, X_test, y_train, y_test) 
+    # a.plot_model(method = 'ols_own', data = 'train', data_dim = 1)
+    a.plot(method = 'ols_skl', data = 'test', data_dim = 1)
+
+    sys.exit()
