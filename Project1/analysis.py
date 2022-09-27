c92433e2bef960a531a894b65adbaa9985e6f8f0
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-09-27 16:12:54 +0200

    Added lasso and ridge

--- Project1/analysis.py
+++ Project1/analysis.py
@@ -8,21 +8,14 @@ import time
 import sys
 import ipdb
 import ols 
+import ridge_regression
+import lasso_regression
 import bdb
 importlib.reload(franke_data)
 importlib.reload(ols)
 plt.style.use('fivethirtyeight')
 plt.style.use('fivethirtyeight')
 
-def r2(y_data, y_model):
-    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
-
-def mse(y_data, y_model):
-    n = np.size(y_model)
-    return np.sum((y_data-y_model)**2)/n
-
-def relative_errror(y_data, y_model):
-    return abs((y_data-y_model)/y_data)
 
 
 @dataclass(frozen=True)
@@ -34,7 +27,7 @@ class Analysis:
     # y_test: np.ndarray
 
 
-    def calculate_loop(self, max_poly_deg:int, score_list = None, method_list = None, data_list = None ):
+    def calculate_loop(self, max_poly_deg:int, score_list = None, method_list = None, data_list = None, lamb: float = 0):
         """ 
         Parameters:
             max_poly_deg (int) - Scores for all polynomial up to degree = max_poly_deg is calculated
@@ -43,13 +36,14 @@ class Analysis:
             data_list (list) - calculate on test and/or training data. values: 'test', 'train'
                                List indices should correspond to method_list
         """
+        assert(len(method_list) == len(data_list))
         poly_score = dict() # Return dict with keys correspodning to polynomial degree
         for deg in range(1, max_poly_deg+1):
-            poly_score[str(deg)] = self.calculate(deg, score_list, method_list, data_list)
+            poly_score[str(deg)] = self.calculate(deg, score_list, method_list, data_list, lamb)
 
         return poly_score
     
-    def calculate(self, deg, score_list = None, method_list = None, data_list = None):
+    def calculate(self, deg, score_list = None, method_list = None, data_list = None, lamb: float = 0):
         """ 
         Parameters:
             deg (int) - Polynomail degree
@@ -80,102 +74,126 @@ class Analysis:
         score_dict = dict()
         for score in score_list: 
             if score == 'mse':
-                score_dict['mse'] = self.__get_mse(deg, method_list, data_list)
+                score_dict['mse'] = self.__get_score(deg, method_list, data_list, lamb, self.mse)
             
             if score == 'r2': 
-                score_dict['r2'] = self.__get_r2(deg, method_list, data_list)
+                score_dict['r2'] = self.__get_score(deg, method_list, data_list, lamb, self.r2)
 
             if score == 'beta':
-                score_dict['beta'] = self.__get_beta(deg, method_list)
+                score_dict['beta'] = self.__get_beta(deg, method_list, lamb)
+
+
 
         return score_dict
 
-
-    def __get_mse(self, deg, method_list, data_list): 
+    def __get_score(self, deg, method_list, data_list, lamb, score_func): 
         X_train_deg = self.franke_object.get_X_train(deg)
         X_test_deg = self.franke_object.get_X_test(deg)
         y_train = self.franke_object.get_y_train()
         y_test = self.franke_object.get_y_test()
-        o = ols.OLS(X_train_deg, y_train)
 
         # Return values 
-        mse_ols = dict()
+        score = dict()
 
         for method, data in zip(method_list, data_list): 
 
             if method == 'ols_own': 
+                o = ols.OLS(X_train_deg, y_train)
                 o.ols()
                 if data == 'train':
                     y_model= o.predict(X_train_deg)
-                    mse_ols['ols_own_train'] = mse(y_train, y_model)
+                    score['ols_own_train'] = score_func(y_train, y_model)
 
                 elif data == 'test': 
                     y_model = o.predict(X_test_deg)
-                    mse_ols['ols_own_test'] = mse(y_test, y_model)
+                    score['ols_own_test'] = score_func(y_test, y_model)
 
             elif method == 'ols_skl': 
+                o = ols.OLS(X_train_deg, y_train)
                 o.skl_ols()
                 if data == 'train':
                     y_model = o.predict(X_train_deg)
-                    mse_ols['ols_skl_train'] = mse(y_train, y_model)
+                    score['ols_skl_train'] = score_func(y_train, y_model)
 
                 elif data == 'test': 
                     y_model = o.predict(X_test_deg)
-                    mse_ols['ols_skl_test'] = mse(y_test, y_model)
+                    score['ols_skl_test'] = score_func(y_test, y_model)
 
-        return mse_ols
+            elif method == 'ridge_own': 
+                r = ridge_regression.RidgeRegression(X_train_deg, y_train, lamb = lamb)
+                r.ridge_own()
+                if data == 'train':
+                    y_model = r.predict(X_train_deg)
+                    score['ridge_own_train'] = score_func(y_train, y_model)
 
-    def __get_r2(self, deg, method_list, data_list): 
+                elif data == 'test': 
+                    y_model = r.predict(X_test_deg)
+                    score['ridge_own_test'] = score_func(y_test, y_model)
+
+            elif method == 'ridge_skl': 
+                r = ridge_regression.RidgeRegression(X_train_deg, y_train, lamb = lamb)
+                r.ridge_skl()
+                if data == 'train':
+                    y_model = r.predict(X_train_deg)
+                    score['ridge_skl_train'] = score_func(y_train, y_model)
+
+                elif data == 'test': 
+                    y_model = r.predict(X_test_deg)
+                    score['ridge_skl_test'] = score_func(y_test, y_model)
+
+            elif method == 'lasso_skl': 
+                l = lasso_regression.LassoRegression(X_train_deg, y_train, lamb = lamb)
+                l.lasso_skl()
+                if data == 'train':
+                    y_model = l.predict(X_train_deg)
+                    score['lasso_skl_train'] = score_func(y_train, y_model)
+
+                elif data == 'test': 
+                    y_model = l.predict(X_test_deg)
+                    score['lasso_skl_test'] = score_func(y_test, y_model)
+
+        return score
+
+    def __get_beta(self, deg, method_list, lamb): 
         X_train_deg = self.franke_object.get_X_train(deg)
-        X_test_deg = self.franke_object.get_X_test(deg)
         y_train = self.franke_object.get_y_train()
-        y_test = self.franke_object.get_y_test()
-        o = ols.OLS(X_train_deg, y_train)
 
         # Return values 
-        r2_ols = dict()
-
-        for method, data in zip(method_list, data_list): 
-
-            if method == 'ols_own': 
-                o.ols()
-                if data == 'train':
-                    y_model= o.predict(X_train_deg)
-                    r2_ols['ols_own_train'] = r2(y_train, y_model)
-
-                elif data == 'test': 
-                    y_model = o.predict(X_test_deg)
-                    r2_ols['ols_own_test'] = r2(y_test, y_model)
-
-            elif method == 'ols_skl': 
-                o.skl_ols()
-                if data == 'train':
-                    y_model = o.predict(X_train_deg)
-                    r2_ols['ols_skl_train'] = r2(y_train, y_model)
-
-                elif data == 'test': 
-                    y_model = o.predict(X_test_deg)
-                    r2_ols['ols_skl_test'] = r2(y_test, y_model)
-
-        return r2_ols
-
-    def __get_beta(self, deg, method_list): 
-        X_train_deg = self.franke_object.get_X_train(deg)
-        y_train = self.franke_object.get_y_train()
-        o = ols.OLS(X_train_deg, y_train)
-
-        # Return values 
-        beta_ols = dict()
+        # beta_ols = dict()
+        beta = dict()
 
         for method in method_list:
-
             if method == 'ols_own': 
-                beta_ols['ols_own_train'] = o.ols()
+                o = ols.OLS(X_train_deg, y_train)
+                beta['ols_own_train'] = o.ols()
 
             elif method == 'ols_skl': 
-                beta_ols['ols_skl_train'] = o.skl_ols()
+                o = ols.OLS(X_train_deg, y_train)
+                beta['ols_skl_train'] = o.skl_ols()
 
-        return beta_ols
+            elif method == 'ridge_own':
+                r = ridge_regression.RidgeRegression(X_train_deg, y_train, lamb = lamb)
+                beta['ridge_own_train'] = r.ridge_own()
+
+            elif method == 'ridge_skl': 
+                r = ridge_regression.RidgeRegression(X_train_deg, y_train, lamb = lamb)
+                beta['ridge_skl_train'] = r.ridge_skl()
+
+            elif method == 'lasso_skl':
+                l = lasso_regression.LassoRegression(X_train_deg, y_train, lamb = lamb)
+                l.lasso_skl()
+
+        return beta
+
+    def r2(self, y_data, y_model):
+        return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
+
+    def mse(self, y_data, y_model):
+        n = np.size(y_model)
+        return np.sum((y_data-y_model)**2)/n
+
+    def relative_errror(self, y_data, y_model):
+        return abs((y_data-y_model)/y_data)
 
 
 if __name__ == '__main__': 
@@ -183,7 +201,7 @@ if __name__ == '__main__':
 
 
 
-    np.random.seed(0)
+    # np.random.seed(0)
     max_poly_deg = 3
     n_data = 1000
     # n_data = 2000000
