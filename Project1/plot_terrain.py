e1373db9604f53947257ff6159b21b6f6fa4912f
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-10-08 12:08:19 +0200

    terrain subfigures

--- Project1/plot_terrain.py
+++ Project1/plot_terrain.py
@@ -20,29 +20,39 @@ plt.style.use('fivethirtyeight')
 class PlotTerrain: 
     terrain_object: terrain_data.TerrainData
 
-    def plot(self, method: str, data_list: list = None): 
+    def plot(self, method: str): 
 
-        fig = plt.figure(figsize=(12,8))
-        ax = fig.add_subplot(111, projection='3d')
+        fig = plt.figure(figsize=(20,8))
+        ax1 = fig.add_subplot(121, projection='3d')
+        ax2 = fig.add_subplot(122, projection='3d')
 
-        # self.__plot_terrain_surface(ax)
-        self.__2d_plot_model(method, ax)
-        if data_list != None: 
-            for data in data_list:
-                # self.__plot_data_points(data, ax)
-                pass
+        self.__plot_terrain_surface(ax1)
+        self.__2d_plot_model(method, ax2, alpha = 1)
+
+        self.__plot_data_points('train', ax1, color = 'y')
+        self.__plot_data_points('test', ax1, color = 'm')
+
+        self.__plot_data_points('train', ax2, color = 'y')
+        self.__plot_data_points('test', ax2, color = 'm')
 
 
             # map(self.__plot_data_points, ax, data_list)
             # self.__plot_data_points(data, ax)
 
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.legend()
+        ax1.set_xlabel('x')
+        ax2.set_xlabel('x')
+        ax1.set_ylabel('y')
+        ax2.set_ylabel('y')
+        ax1.set_zlabel('Height [m]')
+        ax2.set_zlabel('Height [m]')
+        ax1.legend()
+        ax2.legend()
+
+        plt.subplots_adjust(wspace=0.1, hspace=0)
         plt.show()
 
 
-    def __plot_data_points(self, dataset: str, ax):
+    def __plot_data_points(self, dataset: str, ax, color):
         get_func_name = f'get_{dataset}'
         get_data = getattr(self.terrain_object, get_func_name)
         X_data_terrain, y_data_terrain = get_data()
@@ -58,7 +68,7 @@ class PlotTerrain:
         z = z_data[sort_index]
 
         # Plot z data
-        ax.plot(x, y, z, '.', markersize = 2, alpha = 0.5, label = f'{dataset} data')
+        ax.plot(x, y, z, '.', markersize = 5, alpha = 0.5, label = f'{dataset} data', color = color)
 
     def __plot_terrain_surface(self, ax): 
 
@@ -78,13 +88,12 @@ class PlotTerrain:
             z = np.reshape(z, (N,N))
 
         # XXX: sorting
-        surf = ax.plot_surface(x, y, z, label='Terrain Data')#, label = 'Franke function')
+        surf = ax.plot_surface(x, y, z, label='Terrain Data', alpha=1)#, label = 'Franke function')
         surf._edgecolors2d = surf._edgecolor3d
         surf._facecolors2d = surf._facecolor3d
 
 
-    def __2d_plot_model(self, model: str, ax): 
-
+    def __2d_plot_model(self, model: str, ax, alpha): 
         # TODO: in progress
         X_train, y_train = self.terrain_object.get_train()
         if model == 'ols_own': 
@@ -98,15 +107,14 @@ class PlotTerrain:
         else:
             raise ValueError
 
-
         # If 2 dim
-        z_model = o.predict(self.terrain_object.get_X_test())
+        z_model = o.predict(self.terrain_object.get_X_data())
         N = self.terrain_object.N
         Z = z_model.reshape(N, N)
         X = self.terrain_object.x
         Y = self.terrain_object.y
 
-        surf = ax.plot_surface(X, Y, Z, label=f'Model: {model}')#, label = 'Franke function')
+        surf = ax.plot_surface(X, Y, Z, label=f'Model: {model}', alpha = alpha)#, label = 'Franke function')
         surf._edgecolors2d = surf._edgecolor3d
         surf._facecolors2d = surf._facecolor3d
 
@@ -145,7 +153,7 @@ class PlotTerrain:
 
 if __name__ == '__main__': 
 
-    t = terrain_data.TerrainData(5, 500)
+    t = terrain_data.TerrainData(16, 40)
     p = PlotTerrain(t)
     p.plot( method='ols_own', data_list = ['test', 'train'])
 
