f28a49a114ff41d184ba91bef81088ee443952f6
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-10-20 15:43:49 +0200

    get methods

--- Project2/poly_data.py
+++ Project2/poly_data.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn as skl 
+from sklearn.model_selection import train_test_split
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -14,13 +14,36 @@ class PolyData:
     n_data = 100
     coeff: tuple = (1, 0, 1)
 
+    test_size: float = 0.2
+
+    seed = 1
+    set_seed = True
+
     x: np.ndarray = field(init=False, repr=False)
     y: np.ndarray = field(init=False, repr=False)
+
+    X_train: np.ndarray = field(init=False, repr=False)
+    X_test: np.ndarray = field(init=False, repr=False)
+    y_train: np.ndarray = field(init=False, repr=False)
+    y_test: np.ndarray = field(init=False, repr=False)
 
     def __post_init__(self): 
         self.__create_datapoints()
         self.__create_design_matrix()
         self.__create_polynomial()
+        self.__split_data()
+
+    def get_X_trian(self): 
+        return self.X_train
+
+    def get_X_test(self):
+        return self.X_test
+
+    def get_y_train(self):
+        return self.y_train
+
+    def get_y_test(self):
+        return self.y_test
 
     def __create_datapoints(self): 
         x = np.linspace(self.x_range[0], self.x_range[1], self.n_data)
@@ -36,6 +59,22 @@ class PolyData:
             X[:,i] = self.x**i
 
         object.__setattr__(self, 'X', X)
+
+    def __split_data(self): 
+        if self.set_seed:
+            X_train, X_test, y_train, y_test = \
+                    train_test_split(self.X, self.y, test_size=self.test_size, random_state=self.seed)
+        else:
+            X_train, X_test, y_train, y_test = \
+                    train_test_split(self.X, self.y, test_size=self.test_size)
+
+        object.__setattr__(self, 'X_train', X_train)
+        object.__setattr__(self, 'X_test', X_test)
+        object.__setattr__(self, 'y_train', y_train)
+        object.__setattr__(self, 'y_test', y_test)
+
+
+
 
     def plot(self):
         plt.figure()
