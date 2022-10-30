9bdeaf2e79295cea27067bae8e41ec40c9b5c38d
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-10-30 17:29:15 +0100

    test

--- Project2/test_gradientdescent.py
+++ Project2/test_gradientdescent.py
@@ -13,24 +13,47 @@ import plot
 
 
 if __name__ == '__main__':
+    np.random.seed(0)
     reload(poly_data)
     reload(gradient_descent)
     reload(plot)
 
+    eta = 0.15
+    n_epochs = 100
+    n_epochs_sgd = 50
     p = poly_data.PolyData(n_data = 100)
     
-    gd = gradient_descent.GradientDescent(p)
+    gm = gradient_descent.GradientDescent(p)
 
-    gd.plane_gd(0.001)
-    theta_gd = gd.get_thetas()
-    gd.gd_momentum(0.001, 0.1)
-    theta_gd_momentum = gd.get_thetas()
 
-    gd.sgd( 0.001, 20, 50)
-    theta_sgd = gd.get_thetas()
+    # gm.gd(eta, n_epochs, 0.1) 
+    # gd_momentum1 = gm.get_thetas()
+    # gm.gd_momentum(eta, 0.1, n_epochs)
+    # gd_momentum2 = gm.get_thetas()
+
+
+    # gd_dic = {'gd1': gd_momentum1, 'gd2': gd_momentum2}
+    # pl = plot.Plot(p)
+    # pl.plot_mse_vs_theta(gd_dic)
+
+
+
+
+    gm.gd(eta, n_epochs)
+    gd_plain = gm.get_thetas()
+
+    # gm.gd_momentum(eta, 0.1, n_epochs)
+    gm.gd(eta, n_epochs, 0.1) 
+    gd_momentum = gm.get_thetas()
+
+    gm.sgd( eta, 20, n_epochs_sgd)
+    sgd = gm.get_thetas()
+
+    gm.sgd_momentum(eta, 0.1, 20, n_epochs_sgd)
+    sgd_momentum = gm.get_thetas()
 
     # Plotting
-    theta_dict = {'theta_gd': theta_gd, 'theta_gd_momentum': theta_gd_momentum, 'sgd_theta': theta_sgd}
+    theta_dict = {'gd': gd_plain, 'gd_momentum': gd_momentum, 'sgd_theta': sgd, 'sgd_momentum': sgd_momentum}
     pl = plot.Plot(p)
     pl.plot_mse_vs_theta(theta_dict)
 
