58310948a744de4612b420991396546d3153d175
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-10-31 09:55:15 +0100

    Init NN

---
+++ Project2/neural_network.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+import poly_data
+import numpy as np
+import pprint
+import time
+
+
+@dataclass 
+class NeuralNetwork:
+    pass 
+
+@dataclass
+class InputLayer:
+    pass
+
+@dataclass
+class HiddenLayer:
+    pass
+
+@dataclass
+class OuputLayer:
+    pass
+
+@dataclass
+class Node:
+    pass
+
---
+++ Project2/test_neural_network.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+import poly_data
+import numpy as np
+import pprint
+import time
+import neural_network
+from importlib import reload
+
+
+if __name__ == '__main__':
+    reload(neural_network)
+
+
