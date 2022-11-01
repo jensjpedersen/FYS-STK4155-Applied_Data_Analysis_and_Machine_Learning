b84c8676895edacef7f7b8b5538651e32b2fb3a8
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-11-01 11:59:53 +0100

    Fedd Forward NN. TODO: needs testing

--- Project2/neural_network.py
+++ Project2/neural_network.py
@@ -1,3 +1,4 @@
+import abc
 from dataclasses import dataclass, field
 import sys
 import matplotlib.pyplot as plt
@@ -8,21 +9,130 @@ import numpy as np
 import pprint
 import time
 
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+# TODO: incremental id
+@dataclass
+class Layer(abc.ABC):
+
+    @abc.abstractmethod
+    def init_bias_and_weigths(): 
+        " Initialize bias and weigts in layer "
+
+    @abc.abstractmethod
+    def get_output() -> np.ndarray:
+        " Returns output from layer "
+
+    @abc.abstractmethod
+    def feed_forward() -> None:
+        " Calculates ouput from layer "
+
+
+
+@dataclass
+class InputLayer(Layer):
+    X: np.ndarray = field(repr=False)  # Desing matrix with size (n_data, n_features)
+    n_nodes: int = field(init=False) # n_nodes = n_features
+
+    W = None
+    b = None
+    # W: np.ndarray = field(repr=False)
+    # b: np.ndarray = field(repr=False)
+
+    prev_Layer = None
+    next_Layer = object()
+
+    def __post_init__(self): 
+        self.n_nodes = np.shape(self.X)[1]
+
+    def init_bias_and_weigths(self):
+        pass
+
+    def get_output(self): 
+        return self.X
+
+    def feed_forward(self):
+        pass
+
+    
+@dataclass
+class HiddenLayer(Layer):
+    n_nodes: int 
+
+    W: np.ndarray = field(init=False, repr=False)
+    b: np.ndarray = field(init=False, repr=False)
+
+    prev_Layer: Layer = field(init=False, repr=False, default=lambda: object())
+    next_Layer: Layer = field(init=False, repr=False, default=lambda: object())
+
+    a_l: np.ndarray = field(init=False, repr=False, default=None) # output from Activation function. Size (n nodes in layer l-) x (n_data)
+
+    # def __post_init__(self): 
+    #     breakpoint() 
+
+    def init_bias_and_weigths(self):
+        # TODO: check if prev layer is Initialized
+        self.b = np.zeros(self.n_nodes) + 0.01
+        self.W = np.random.randn(self.prev_Layer.n_nodes, self.n_nodes)
+
+    def get_output(self):
+        if np.size(self.a_l) == 1: 
+            raise ValueError('Call method feed_forward to initlize variable')
+
+        return self.a_l
+
+    def feed_forward(self) -> None:
+        output_prev_layer = self.prev_Layer.get_output()
+        z_l = output_prev_layer @ self.W + self.b # Feed forward
+        a_l = sigmoid(z_l)  # Activation func
+        self.a_l = a_l
+
+@dataclass
+class OuputLayer(Layer):
+    n_nodes: int # n_nodes = n_targets
+
+    W: np.ndarray = field(init=False, repr=False)
+    b: np.ndarray = field(init=False, repr=False)
+
+    prev_Layer: Layer = field(init=False, repr=False, default=lambda: object())
+    next_Layer = None
+
+    a_l: np.ndarray = field(repr=False, default=None) # output from Activation function. Size (n nodes in layer l-) x (n_data)
+                           # Initialize varablie with method: feed_forward
+    # def __post_init__(self):
+
+    def init_bias_and_weigths(self): 
+        self.b = np.zeros(self.n_nodes) + 0.01
+        self.W = np.random.randn(self.prev_Layer.n_nodes, self.n_nodes)
+
+    def get_output(self):
+        if np.size(self.a_l) == 1: 
+            raise ValueError('Call method feed_forward to initlize variable')
+
+        return self.a_l
+
+    def feed_forward(self) -> None:
+        output_prev_layer = self.prev_Layer.get_ouput()
+        z_l = output_prev_layer @ self.W + self.b # Feed forward
+        a_l = sigmoid(z_l)  # Activation func
+        self.a_l = a_l
 
 @dataclass 
 class NeuralNetwork:
     data_object: object 
     n_hidden_layers: int
     n_nodes_per_hidden_layer: int
+    # n_targets: int = field(init=False)
+    n_targets: int
 
     n_features: int = field(init=False)
     n_data: int = field(init=False)
-    n_targets: int = field(init=False)
 
     X_data: np.ndarray = field(init=False, repr=False)
     y_data: np.ndarray = field(init=False, repr=False)
 
-    Layers: list = field(default_factory=lambda: [])
+    Layers: Layer = field(default_factory=lambda: [])
     def __post_init__(self):
         self.X_data, self.y_data = self.data_object.get_train()
 
@@ -30,68 +140,85 @@ class NeuralNetwork:
         self.n_data = self.X_data.shape[0]
         self.n_targets = 10 # XXX: Automatize 
 
-
         self.__init_layers()
 
 
     def __init_layers(self): 
         # Init input layer
-        self.Layers.append(self.__input_layer(self.n_features))
+
+        # Get input data
+        # TODO replace __input_layer, __input_layer, __output_layer
+        input_Layer = self.__input_layer()
+        self.Layers.append(input_Layer)
 
         # Init hidden layers
         for i in range(self.n_hidden_layers):
-            self.Layers.append(self.__hidden_layer(self.n_nodes_per_hidden_layer))
+            # self.Layers.append(self.__hidden_layer(self.n_nodes_per_hidden_layer))
+            print(f'i = {i}')
+            print(f'selv.n_nodes_per_hidden_layer = {self.n_nodes_per_hidden_layer}')
+            
+            
+            self.Layers.append(HiddenLayer( self.n_nodes_per_hidden_layer))
 
         # Init ouput layer
         self.Layers.append(self.__output_layer(self.n_targets))
 
+        # Connect layers
+        for i, Layer in enumerate(self.Layers):
+            if Layer.next_Layer != None:
+                Layer.next_Layer = self.Layers[i+1]
+
+            if Layer.prev_Layer != None:
+                Layer.prev_Layer = self.Layers[i-1]
+
+        # Initialize bias and weigts
+        for i, layer in enumerate(self.Layers): 
+            layer.init_bias_and_weigths()
+
+
+
+
+    def __input_layer(self): 
+        """Returns input layer object"""
+        il = InputLayer(self.X_data)
+        return il
 
     def __hidden_layer(self, n_nodes: int): 
         """Returns hidden layer object"""
-        W = np.random.randn(self.n_features, n_nodes)
-        b = np.zeros(self.n_nodes_per_hidden_layer) + 0.01
-        hl = HiddenLayer(W, b)
+        # W = np.random.randn(self.n_features, n_nodes)
+        # b = np.zeros(self.n_nodes_per_hidden_layer) + 0.01
+        hl = HiddenLayer(n_nodes)
         return hl
 
-    def __input_layer(self, n_nodes: int): 
-        """Returns input layer object"""
-        # Init hidden weigts and bias
-        W = np.random.randn(self.n_features, n_nodes)
-        b = np.zeros(self.n_nodes_per_hidden_layer) + 0.01
-        il = InputLayer(W, b)
-        return il
 
-    def __output_layer(self, n_nodes: int):
+    def __output_layer(self, n_targets: int):
         """Returns output layer object"""
-        W = np.random.randn(self.n_features, n_nodes)
-        b = np.zeros(self.n_nodes_per_hidden_layer) + 0.01
-        ol = OuputLayer(W, b)
+        # W = np.random.randn(self.n_nodes_per_hidden_layer, n_nodes)
+        # b = np.zeros(self.n_nodes_per_hidden_layer) + 0.01
+        ol = OuputLayer(n_targets)
         return ol
 
-
-
+    def get_layers(self, l: int = None):
+        if l == None: 
+            return self.Layers
+        return self.Layers[l]
 
 
 @dataclass
-class InputLayer:
-    W: np.ndarray = field(repr=False)
-    b: np.ndarray = field(repr=False)
+class TrainNetwork: 
+    nn: NeuralNetwork
+
+
+    def feed_forward(self):
+        layers = self.nn.get_layers()
+
+        for l, layer in enumerate(layers):
+            layer.feed_forward()
+            output = layer.get_output()
+
+        return output
+
+
     
-@dataclass
-class HiddenLayer:
-    W: np.ndarray = field(repr=False)
-    b: np.ndarray = field(repr=False)
-
-    def update_weights(self): 
-        pass
-
-    def update_bias(self):
-        pass
-
-
-@dataclass
-class OuputLayer:
-    W: np.ndarray = field(repr=False)
-    b: np.ndarray = field(repr=False)
 
 
--- Project2/test_neural_network.py
+++ Project2/test_neural_network.py
@@ -10,8 +10,59 @@ import time
 import neural_network
 from importlib import reload
 
+def test_layers(nn: neural_network.NeuralNetwork):
+    ' Check that layers are linked correct '
+
+    layers = nn.get_layers()
+
+    assert(layers[0].prev_Layer == None)
+    assert(layers[-1].next_Layer == None)
+
+    for i in range(len(layers)-1):
+        assert(id(layers[i].next_Layer) == id(layers[i+1]))
+
+    for i in range(1, len(layers)):
+        assert(id(layers[i].prev_Layer) == id(layers[i-1]))
+
+    print(' l:      prev_Layer    | layer     | next_Layer')
+    for i, layer in enumerate(layers): 
+        print(f' {i}: {id(layer.prev_Layer)} | {id(layer)} | {id(layer.next_Layer)}')
+
+    print('Test layers OK')
+
+def test_feed_forward(nn: neural_network.NeuralNetwork):
+    # Print matrix sizes
+    layers = nn.get_layers()
+    print(' z_l    =       a^l  @ W^l   +    b^l')
+    for l, layer in enumerate(layers): 
+        print(f'z_{l}: {np.shape(layer.W)}   empty  {np.shape(layer.b)}')
+
+
+
+
+
+    
 
 if __name__ == '__main__':
     reload(neural_network)
+
+    reload(poly_data)
+    p = poly_data.PolyData(n_data=100)
+
+    depth = 2
+    width = 10
+    n_targets = 3
+    nn = neural_network.NeuralNetwork(p, depth, width, n_targets)
+
+    l1 = nn.get_layers(0)
+    l2 = nn.get_layers(1)
+    l3 = nn.get_layers(2)
+    l4 = nn.get_layers(3)
+
+    # test_feed_forward(nn)
+    # tn = neural_network.TrainNetwork(nn)
+    # tn.feed_forward()
+
+    
 
 
