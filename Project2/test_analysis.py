e4e9e0cbb424b5dfc3bec6e5e5aec45c3ca1e53a
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-11-12 14:08:59 +0100

    Current results for cancer data.

--- Project2/test_analysis.py
+++ Project2/test_analysis.py
@@ -55,37 +55,9 @@ if __name__ == '__main__':
         "gamma": 0.9,
         "lambd": 0,
         "tuning_method": 'none',
-        "n_minibatches": 10,
-        "epochs": 100
+        "n_minibatches": 20,
+        "epochs": 400
         }
-
-    # Example: heatmap with eta and lambda
-    # a = analysis.Analysis(**parameters)
-    # a.eta = np.logspace(-5, -2, 4)
-    # a.lambd = np.logspace(-5, -2, 4)
-    # a.plot_heatmap()
-    
-
-    # Example: heatmap with width and depth
-    # a = analysis.Analysis(**parameters)
-    # a.width = [5, 10, 15, 20]
-    # a.depth = [0, 1, 2, 3]
-    # a.plot_heatmap()
-
-    # Plot cost score for differnet tuning 
-    # a = analysis.Analysis(**parameters)
-    # a.tuning_method = ['none', 'rms_prop', 'adam']
-    # a.plot_score('cost')
-
-    # a = analysis.Analysis(**parameters)
-    # a.activation_hidden = ['sigmoid', 'relu', 'leaky_relu']
-    # # a.activation_hidden = 'relu'
-    # # a.eta = np.logspace(-5, -1, 5)
-    # # a.plot_heatmap()
-    # # a.plot_score('cost')
-    # a.epochs = 50
-    # a.plot_score('accuracy')
-
 
 
 #  ____            _         _ 
@@ -93,13 +65,98 @@ if __name__ == '__main__':
 # | |_) / _` | '__| __|  / _` |
 # |  __/ (_| | |  | |_  | (_| |
 # |_|   \__,_|_|   \__|  \__,_|
-                             
-    # find optimal number of n_minibatches
+    # a = analysis.Analysis(**parameters)
+    # # find optimal number of n_minibatches
+    # a.epochs = 400
+    # a.n_minibatches = [1, 5, 10, 15, 20]
+    # a.plot_score('accuracy')
+
+    # # Find optimal values fro lmabda and eta
+    # a = analysis.Analysis(**parameters) # XXX: in progress
+    # a.epochs = 200  # XXX: Reduced number of ephcos to speedup
+    # a.lambd = np.logspace(-5, 0, 6)
+    # a.lambd[-1] = 0
+    # a.eta = np.logspace(-5, -1, 5)
+    # a.plot_heatmap('accuracy')
+
+
+    # Use new best parameters for lambd and eta
+    parameters = {
+        "X_train": X_train,
+        "y_train": y_train,
+        "X_test": X_test,
+        "y_test": y_test,
+        "eta": 0.1,  # XXX: changed
+        "depth": 1 ,
+        "width": 10,
+        "n_output_nodes": 1,
+        "cost_score": 'cross_entropy',
+        "activation_hidden": 'sigmoid',
+        "activation_output": 'sigmoid',
+        "gamma": 0.9,
+        "lambd": 0.1,  # XXX: changed
+        "tuning_method": 'none',
+        "n_minibatches": 20,
+        "epochs": 200 # XXX: changed
+        }
+
+    # # Example: heatmap with width and depth
+    # a = analysis.Analysis(**parameters)
+    # a.width = [5, 10, 15, 20]
+    # a.depth = [1, 2, 3]
+    # a.plot_heatmap('accuracy')
+
+
+    # # Example: heatmap with width and depth
+    # a = analysis.Analysis(**parameters)
+    # a.lambd = 0
+    # a.width = [5, 10, 15, 20]
+    # a.depth = [1, 2, 3]
+    # a.plot_heatmap('accuracy')
+
+
+    # Use new parameters for network dept and width
+    parameters = {
+        "X_train": X_train,
+        "y_train": y_train,
+        "X_test": X_test,
+        "y_test": y_test,
+        "eta": 0.1,  
+        "depth": 2 ,  # XXX: changed 
+        "width": 5,  # XXX: changed
+        "n_output_nodes": 1,
+        "cost_score": 'cross_entropy',
+        "activation_hidden": 'sigmoid',
+        "activation_output": 'sigmoid',
+        "gamma": 0.9,
+        "lambd": 0.1, 
+        "tuning_method": 'none',
+        "n_minibatches": 20,
+        "epochs": 200 
+        }
+
+
+    # a = analysis.Analysis(**parameters)
+    # a.lambd = 0
+    # a.eta = 0.001 # Low eta to compare convergence rate
+    # a.lambd = [0, 1e-3, 1e-2, 0.1]
+    # a.plot_score('accuracy')
+
+
+    # Test different learning rate tuning 
     a = analysis.Analysis(**parameters)
-    a.n_minibatches = [1, 5, 10, 15, 20]
+    a.lambd = 0
+    a.eta = 0.001 # Low eta to compare convergence rate
+    a.tuning_method = ['none', 'adam', 'rms_prop', 'adagrad']
     a.plot_score('accuracy')
+
+    
+
+    # a.activation_hidden = ['sigmoid', 'relu', 'leaky_relu']
+
 
 
     
 
+    
 
